250d50d299fe69b315809953557039cbf9eadb1c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-12-30 21:32:27 +0900

    Fill the commentary of the file header

--- initchart.el
+++ initchart.el
@@ -20,7 +20,28 @@
 
 ;;; Commentary:
 
+;; initchart.el provides macros and functions to measure and visualize a init
+;; process of Emacs.
 ;; 
+;; First, one need to record the execution time of some primary functions such
+;; as 'load' and 'require'.  Use the macro 'record-execution-time-of' at the
+;; beginning of your init.el to register functions of concern, and then launch
+;; an Emacs process as usual.  Execution time of the registered functions will
+;; be logged in the '*Messages*' buffer.
+;; 
+;; Then, you can visualize these logs by invoking the command
+;; 'visualize-init-sequence', which will ask you the filepath to save an output
+;; SVG image.
+;; 
+;; Example:
+;;   (require 'initchart)
+;;   (record-execution-time-of load file)
+;;   (record-execution-time-of require feature)
+;;   (add-hook 'after-init-hook
+;;             (lambda ()
+;;               (message "exec-time: init %f %f"
+;;                        (float-time before-init-time)
+;;                        (float-time after-init-time))))
 
 ;;; Code:
 
