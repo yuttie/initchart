15a5cf988e6d978617a052d0f961ba995cb2f528
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2014-01-08 13:06:04 +0900

    Trivial change

--- initchart.el
+++ initchart.el
@@ -1,4 +1,4 @@
-;;; initchart.el --- Emacs' init process performance visualization  -*- lexical-binding: t; -*-
+;;; initchart.el --- Emacs' init process performance visualization  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2013  Yuta Taniguchi
 
